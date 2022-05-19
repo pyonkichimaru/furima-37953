4df0acba7b188fa1449be338b74f8ef53c30daa6
pyonkichimaru <65487947+pyonkichimaru@users.noreply.github.com>
2022-05-19 19:32:02 +0900

    修正

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -3,12 +3,6 @@ class FurimasController < ApplicationController
   def index
   end
   
-  def new
-    @furima=Furima.new
-  end
-
-  def create
-    @furima = Furima.new
-  end
+  
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :nickname,        presence: true
-         validates :email,           presence: true
-         validates :password,        presence: true
-         validates :first_name,      presence: true
-         validates :last_name,       presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana,  presence: true
-         validates :password,        presence: true
+         validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+         validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
          validates :birth_date,      presence: true
-  has_many :products
-  has_many :buys       
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX 
+  #has_many :products
+  #has_many :buys       
 end
