941541f2dbe3b56b8289398cf95d54a2c219ed5c
pyonkichimaru <65487947+pyonkichimaru@users.noreply.github.com>
2022-06-03 17:23:09 +0900

    コードレビューの修正2

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,6 @@
 class Address < ApplicationRecord
 
-  #validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  #validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
-
+  
   belongs_to :buy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
